e00349432cd5888c4c3957d8580cf94320f4da0e
paula <paulalfrex@gmail.com>
2022-04-28 16:28:15 +0200

    Removed usles Functions and added longer watch period

--- calculator.py
+++ calculator.py
@@ -2,14 +2,5 @@
 def liveCalc(last,div):
     return (last/div)/60
 
-def calc5Min(last,change5):
-    return (last/(sum(change5)/len(change5)))/60
-
-def calc10Min(last,change10):
-    return (last/(sum(change10)/len(change10)))/60
-
-def calc30Min(last,change30):
-    return (last/(sum(change30)/len(change30)))/60
-
-def calc60Min(last,change60):
-    return (last/(sum(change60)/len(change60)))/60
+def calcCustomMin(last,changes):
+    return (last/(sum(changes)/len(changes)))/60
--- main.py
+++ main.py
@@ -15,26 +15,31 @@ while True:
     if len(left) == 0:
         left.append(c.getCurent())
     else:
-        if len(left) >= 120:
-            left = left[1:120]
+        if len(left) >= 600:
+            left = left[1:600]
             left.append(c.getCurent())
         else:
             left.append(c.getCurent())
-        if len(changes) >= 10:
-            changes = changes[1:120]
+        if len(changes) >= 600:
+            changes = changes[1:600]
             changes.append(left[len(left) - 2] - left[len(left) - 1])
         else:
             changes.append(left[len(left) - 2] - left[len(left) - 1])
     if len(changes):
         print("Current estimate:",liveCalc(c.getCurent(),changes[len(changes)-1]))
     if len(changes) >= 5:
-        print("5 Minit Estimate:", calc5Min(c.getCurent(), changes[len(changes)-5:]))
+        print("5 Minit Estimate:", calcCustomMin(c.getCurent(),changes[len(changes)-5:]))
     if len(changes) >= 10:
-        print("10 Minit Estimate:",calc10Min(c.getCurent(),changes[len(changes)-10:]))
+        print("10 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-10:]))
     if len(changes) >= 30:
-        print("30 Minit Estimate:",calc30Min(c.getCurent(),changes[len(changes)-30]))
+        print("30 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-30]))
     if len(changes) >= 60:
-        print("60 Minit Estimate:",calc60Min(c.getCurent(),changes[len(changes)-60:]))
-
+        print("60 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-60:]))
+    if len(changes) >= 120:
+        print("120 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-120:]))
+    if len(changes) >= 240:
+        print("240 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-240:]))
+    if len(changes) >= 480:
+        print("480 Minit Estimate:",calcCustomMin(c.getCurent(),changes[len(changes)-480:]))
 
     time.sleep(60)
